a5d8b0d792269562c2aa4a689a1ec1c3713cc4a9
joceano <joceano@viasoft.com.br>
2021-03-18 08:09:21 -0300

    Implementado o módulo de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,34 +13,30 @@ import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton
 import { DropdownModule } from 'primeng/components/dropdown/dropdown';
 import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
 
-import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { LancamentosModule } from './lancamentos/lancamentos.module';
 
 import { AppComponent } from './app.component';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { MessageComponent } from './message/message.component';
-import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
 import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LancamentosPesquisaComponent,
     NavbarComponent,
     PessoasPesquisaComponent,
-    LancamentoCadastroComponent,
     PessoaCadastroComponent,
     MessageComponent,
-    LancamentosGridComponent,
     PessoasGridComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+
+    LancamentosModule,
 
     InputTextModule,
     ButtonModule,
@@ -50,9 +46,7 @@ import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
     CalendarModule,
     SelectButtonModule,
     DropdownModule,
-    InputMaskModule,
-
-    CurrencyMaskModule
+    InputMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
