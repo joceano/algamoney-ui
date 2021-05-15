cdf8ac543d4dc7218df5ad9a90b569399cea89b2
joceano <alves.joceano@gmail.com>
2021-05-15 14:59:14 -0300

    Implementado a exclusão do anexo.

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -76,6 +76,13 @@ export class LancamentoCadastroComponent implements OnInit {
     return `https://${this.formulario.get('urlAnexo').value}`;
   }
 
+  removerAnexo() {
+    this.formulario.patchValue({
+      anexo: null,
+      urlAnexo: null
+    });
+  }
+
   get nomeAnexo() {
     const nome = this.formulario.get('anexo').value;
     if (nome) {
