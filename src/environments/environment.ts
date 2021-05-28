d3ddb2c592d34351ec26cd38bbd11b33ed794233
joceano <alves.joceano@gmail.com>
2021-05-27 22:10:46 -0300

    Correção

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,6 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
 
-  tokenWhitelistedDomains: [ new RegExp('/localhost:8080/') ],
+  tokenWhitelistedDomains: [ new RegExp('localhost:8080') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
